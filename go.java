36a90e9989efc0a834886f1f8849e82f2a5789ea
Alshaymaa allahim <133970243+Alshaymaalfahad@users.noreply.github.com>
2023-09-11 00:11:15 +0300

    Update go.java

---
+++ go.java
@@ -0,0 +1,13 @@
+import javax.swing.event.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+ 
+public class go {
+public static void main(String[]args) {
+       projectData inter1=new projectData();
+       inter1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       inter1.setSize(10000,10000);
+       inter1.setVisible(true);
+}
+}
